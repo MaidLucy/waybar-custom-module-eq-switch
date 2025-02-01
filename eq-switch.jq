eceb6e449696ff5840359d9b3f5385becf8dadfe
Lucy <maidlucy@pm.me>
2025-02-01 16:45:42 +0100

    add functionality to increase or decrease sub volume

--- eq-switch.jq
+++ eq-switch.jq
@@ -15,11 +15,25 @@ map(. | select(
 	        "key": "LSP Parametric Equalizer x16 Stereo:enabled"
 	    })
         )) |
-	(.[0].value == 1)
+	(.[0].value == 1),
+    "sub_vol": [ .info.params.Props.[].params ] | flatten(1) |
+	[. as $arr | range(0; length; 2) | {
+	    "key": ($arr[.]),
+	    "value": $arr[. + 1]
+	}] |
+	map(. | select(
+            contains({
+	        "key": "LSP Crossover Stereo x8:bg_0"
+	    })
+        )) |
+	(.[0].value | log10 | . * 20 | round)
 } |
 {
     "id": .id,
     "text": .id,
+    "sub_vol": .sub_vol,
+    "sub_p1": pow(10; (.sub_vol + 1) / 20),
+    "sub_m1": pow(10; (.sub_vol - 1) / 20),
     "percentage": (if .eq_enabled == true then 100 else 0 end),
     "alt": (if .eq_enabled == true then "enabled" else "disabled" end),
     "class": (if .eq_enabled == true then "enabled" else "disabled" end),
--- eq-switch.sh
+++ eq-switch.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-json=$(pw-dump | jq -f ~/Software/eq-switch.jq)
+json=$(pw-dump | jq -f ./eq-switch.jq)
 node_id=$(echo $json | jq '.id')
-set_to=$(echo $json | jq '.inverse')
-pw-cli s $node_id Props '{ params = [ "LSP Parametric Equalizer x16 Stereo:enabled" '$set_to' ]}' > /dev/null
+eq_switch=$(echo $json | jq '.inverse')
+b_inc=$(echo $json | jq '.sub_p1')
+b_dec=$(echo $json | jq '.sub_m1')
+if [ $1 == "sub+" ]; then
+    pw-cli s $node_id Props '{ params = [ "LSP Crossover Stereo x8:bg_0" '$b_dec' ]}' > /dev/null
+else if [ $1 == "sub-" ]; then
+    pw-cli s $node_id Props '{ params = [ "LSP Crossover Stereo x8:bg_0" '$b_inc' ]}' > /dev/null
+else if [ $1 == "eqt" ]; then
+    pw-cli s $node_id Props '{ params = [ "LSP Parametric Equalizer x16 Stereo:enabled" '$eq_switch' ]}' > /dev/null
+fi fi fi
 sleep 0.1
-pw-dump | jq -f ./Software/eq-switch.jq --compact-output --unbuffered
+pw-dump | jq -f ./eq-switch.jq --compact-output --unbuffered
